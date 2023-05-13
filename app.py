af066f1621039fc6d8ef16d7b59aba19a0fccce4
Leon Tzu Chiang Chan <leontzuchiangchan@leons-air.bmcc.cuny.edu>
2023-05-13 11:43:19 -0400

    characters added

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ print("script loaded")
 def emperor(name, weapon):
     this_name = (name,)
     this_weapon = (weapon,)
+
+
+Wei = emperor('Cao Cao'),
+Shu = emperor('Liu Bei'),
+Wu = emperor('Sun Quan'),
